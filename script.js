41f3cd294580d92401fe802d3fda91035643354b
Helenalisenok <helena.lisenok@interia.pl>
2022-10-31 15:58:46 +0100

    31.10

--- script.js
+++ script.js
@@ -70,6 +70,151 @@ function checkResult1() {
             out_arr5.innerHTML = elements;
             
           }
+          function checkResult6() {
+            let value6 = document.getElementById('t7').value;
+            let value7 = document.getElementById('t8').value;
+            value6 = Number(value6);
+            value7 = Number(value7);
+            let arr4 = [];
+            let i1=value6;
+            while (i1<=value7){
+            let total3 = arr4.push(i1);
+            i1++;
+            }
+            let out_arr6 = document.getElementById('out_arr6');
+            out_arr6.innerHTML = arr4;
+            console.log(arr4);
+  }
+
+            function checkResult7() {
+            let value8 = document.getElementById('t9').value;
+            value8 = Number(value8);
+            let mult = value8;
+            let iter_ammount = 0;
+            while (mult < 1000) { 
+            mult = mult*3;
+            iter_ammount = iter_ammount+1;
+        }
+          console.log(mult);
+          console.log(iter_ammount);
+} 
+         
+        function checkResult8() {
+        let char_arr = ['a', 'b', 'c', 'd', 'e'];
+        for (let i2 = 0; i2 <= char_arr.length - 1; i2++) {
+          console.log(char_arr[i2]);
+          }
+        }
+        function checkResult9() {
+          let char_arr1 = ['a', 'b', 'c', 'd', 'e'];
+          for (let i3 = char_arr1.length - 1; i3 > 0; i3--) {
+            console.log(char_arr1[i3]);
+            }
+          } 
+          function checkResult10() {
+            let char_arr2 = ['a', 'b', 'c', 'd', 'e'];
+            for (let i4 = 1; i4 <= char_arr2.length - 2; i4++) {
+              console.log(char_arr2[i4]);
+              }
+            } 
+            function checkResult11() {
+               let char_arr3 = ['a', 'b', 'c', 'd', 'e'];
+               for (let i5 = 0; i5 >= char_arr3.length-1; i5++) {
+                console.log(char_arr3[i5]);
+              }
+              } 
+              function checkResult12() {
+                let char_arr4 = [2, 5, 9, 15, 1, 4];
+                for (let elem1 of char_arr4) {
+                  if (elem1>3 && elem1<10)
+                 console.log(elem1);
+               }
+               } 
+               function checkResult13() {
+                let obj1 = {a: 1, b: 2, c: 3, d: 4, e: 5};
+                 for (var elem2 in obj1) {
+                  if (obj1[elem2] % 2 !== 0)
+                 console.log(obj1[elem2]);
+               }
+               } 
+               function checkResult14() {
+                let sum = 0;
+                for (let i = 2; i <= 100; i++) {
+                  if (i%2 ==0)
+                  sum = sum + i;
+                  }
+                  console.log(sum);
+                } 
+                function checkResult15() {
+                  let sum = 0;
+                  for (let i = 1; i <= 99; i++) {
+                    if (i%2 !==0)
+                    sum = sum + i;
+                    }
+                    console.log(sum);
+                  } 
+                  function checkResult16() {
+                    let mult = 1;
+                    for (let i = 1; i <= 20; i++) {
+                      mult = mult*i;
+                      }
+                      console.log(mult);
+                    } 
+                    function checkResult17() {
+                      let sum1 = 0;
+                      let arr5 = [2, 5, 9, 3, 1, 4];
+                      for (let elem3 of arr5) {
+                        sum1 += elem3;
+                      }
+                      
+                      console.log(sum1); // искомая сумма
+                      } 
+                    function checkResult18() {
+                      let sum2 = 0;
+                      let arr6 = [2, 5, 9, 3, 1, 4];
+                      for (let elem4 of arr6) {
+                        if (elem4%2 == 0)
+                        sum2 += elem4;
+                      }
+                      console.log(sum2); // искомая сумма
+                        } 
+
+                  function checkResult19() {
+                    let sum3 = 0;
+                    let arr7 = [2, 5, 9, 3, 1, 4];
+                    for (let elem5 of arr7) {
+                        sum3 += elem5*elem5;
+                       }
+                    console.log(sum3); 
+                        } 
+                  function checkResult20() {
+                      let mult1 = 1;
+                      let arr8 = [2, 5, 9, 3, 1, 4];
+                      for (let elem6 of arr8) {
+                      mult1 = mult1*elem6;
+                            }
+                      console.log(mult1); 
+                              } 
+                  function checkResult21() {
+                    let str = '';
+                    for (let i = 1; i <= 5; i++) {
+                      str += '-';
+                    }
+                    console.log(str);
+                    
+                        }       
+                function checkResult22() {
+                  let arr9 = [];
+
+                  for (let i = 0; i <= 10; i=i+2) {
+                    arr9[i] = '-';
+                    i1 = String(i);
+                    arr9.push(i1);
+                    console.log(arr9[i]);
+                  }
+                                 
+               
+                }                                
   window.onload = function () {
   let codeBox1 = document.getElementById('code1');
   codeBox1.innerHTML = checkResult1.toString();
@@ -81,5 +226,7 @@ function checkResult1() {
   codeBox4.innerHTML = checkResult4.toString();
   let codeBox5 = document.getElementById('code5');
   codeBox5.innerHTML = checkResult5.toString();
+  let codeBox6 = document.getElementById('code6');
+  codeBox6.innerHTML = checkResult6.toString();
   }
  
